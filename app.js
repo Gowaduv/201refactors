002de58155ee268a47aed033aba22a6a327865a0
KMSkelton <kmskelton@outlook.com>
2016-01-25 14:34:53 -0800

    cells are editable on click

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var storeData = [];
 var storeHours = ["5AM", "6AM", "7AM", "8AM", "9AM", "10AM", "11AM", "Noon", "1PM", "2PM", "3PM", "4PM", "5PM", "6PM", "7PM", "8PM"];
+
 function Store(storeName, minCust, maxCust, lbsSold) {
   this.storeName = storeName;
   this.minCust = minCust;
@@ -19,6 +20,7 @@ Store.prototype.hourlyLbsGen = function() {
     this.hourlyLbs.push((this.hourlyCustomers[i] * this.lbsSold).toFixed(1));
   }
 }
+
 function renderFirstRow() {
   var table = document.getElementById("tableJS");
   var trEl = document.createElement("tr");
@@ -38,11 +40,13 @@ Store.prototype.render = function() {
   var table = document.getElementById("tableJS");
   var trEl2 = document.createElement("tr");
   var tdEl = document.createElement("td");
+  tdEl.setAttribute("contentEditable", "true");
   tdEl.textContent = this.storeName;
   trEl2.appendChild(tdEl);
   table.appendChild(trEl2);
   for (var i = 0; i < this.hourlyCustomers.length; i++) {
     var tdEl = document.createElement("td");
+    tdEl.setAttribute("contentEditable", "true");
     tdEl.textContent = this.hourlyLbs[i];
     trEl2.appendChild(tdEl);
   }
@@ -52,8 +56,7 @@ var pikePlace = new Store("Pike Place", 30, 110, 0.75);
 
 function submitData(event) {
   event.preventDefault();
-  if (!event.target.newLocationName.value || !event.target.fewestCust.value || !event.target.mostCust.value || !event.target.avgPurch.value)
-  {
+  if (!event.target.newLocationName.value || !event.target.fewestCust.value || !event.target.mostCust.value || !event.target.avgPurch.value) {
     return alert("All fields must be completed.");
   } else {
     var newLocation = event.target.newLocationName.value;
@@ -62,10 +65,10 @@ function submitData(event) {
     var avgSales = parseInt(event.target.avgPurch.value);
     var newStore = new Store(newLocation, minCust, maxCust, avgSales);
     storeData.push(newStore);
-event.target.newLocationName.value = null;
-event.target.fewestCust.value = null;
-event.target.mostCust.value = null;
-event.target.avgPurch.value = null;
+    event.target.newLocationName.value = null;
+    event.target.fewestCust.value = null;
+    event.target.mostCust.value = null;
+    event.target.avgPurch.value = null;
   }
 }
 document.getElementById("addStore").addEventListener("submit", submitData, false);
